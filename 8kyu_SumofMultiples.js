cb7fbcc23c1f6ffdfd1c231feca40aedc63f3377
Thomas Kjærulff <thomas.kjaerulff@fellowmind.dk>
2023-06-27 10:25:37 +0200

    Initial commit

---
+++ 8kyu_SumofMultiples.js
@@ -0,0 +1,20 @@
+// 8 kyu - Sum of Multiples (https://www.codewars.com/kata/57241e0f440cd279b5000829/train/javascript)
+
+/*
+Your Job
+Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+
+Examples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+*/
+
+function sumMul(n,m) {
+    
+}
